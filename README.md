7e8fb3dd998af143df993985c2ecb4406c71919e
Felicity Liu <felicityliu0410@gmail.com>
2023-01-07 14:52:56 +1030

    delete some files

--- README.md
+++
@@ -1,2 +0,0 @@
-# weather-app-react-project2
-JR fullstack course project2
---
+++ weather-forecast-app/src/components/Weather/Weather.js
@@ -0,0 +1,11 @@
+import './Weather.css'
+
+const Weather = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Weather
